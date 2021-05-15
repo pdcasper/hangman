867bd059543b77231fc76eba6382b5df1ee331b3
pdcasper@gmail.com <pdcasper@gmail.com>
2021-05-15 12:59:58 +0200

    hangman images

--- src/components/HangmanPicture.tsx
+++ src/components/HangmanPicture.tsx
@@ -1,16 +1,23 @@
 import { render } from '@testing-library/react';
 import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import logo from '../logo.svg';
+import img00 from '../hang00.jpg';
+import img01 from '../hang01.jpg';
+import img02 from '../hang02.jpg';
+import img03 from '../hang03.jpg';
+import img04 from '../hang04.jpg';
+import img05 from '../hang05.jpg';
+import img06 from '../hang06.jpg';
+import img07 from '../hang07.jpg';
 
 interface PictureProperties {
     errorCont: number
 }
 export function Picture(props: PictureProperties) {
+    const images =[img00, img01, img02, img03, img04, img05, img06, img07];
+    
     return(
         <React.Fragment>
-            <span>Errores {props.errorCont}</span>
-            <img src={logo}></img>
+            <img src={images[props.errorCont]} style={{height: 800}}></img>
         </React.Fragment>
     )
 }
