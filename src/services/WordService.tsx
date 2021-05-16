bc8fb0688662b5482260b9a536c751079d96de11
pdcasper <pdcasper@gmail.com>
2021-05-16 16:38:29 +0200

    Added list of words in spanish

--- src/services/WordService.tsx
+++ src/services/WordService.tsx
@@ -1,5 +1,9 @@
+
 export class WordService {
     getWord() : string {
-        return "WORDSERVICE";
+        let words = require("an-array-of-spanish-words");
+        let index = Math.round(Math.random() * (words.length));
+        console.log(words[index]);     
+        return words[index].toUpperCase();
     }
 }
